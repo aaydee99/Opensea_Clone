df24fc05a3b1a14f6f67858f0713528403eaad6d
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2022-07-06 01:01:07 +0500

    final Commit

---
+++ components/nft/itemActivity/EventItem.js
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import { BsFillCartFill } from 'react-icons/bs'
+import EthLogo from '../../../assets/images/eth.png'
+const style = {
+  eventItem: `flex px-4 py-5 font-medium`,
+  event: `flex items-center`,
+  eventIcon: `mr-2 text-xl`,
+  eventName: `text-lg font-semibold`,
+  eventPrice: `flex items-center`,
+  eventPriceValue: `text-lg`,
+  ethLogo: `h-5 mr-2`,
+  accent: `text-[#2081e2]`,
+}
+
+const EventItem = ({ event }) => {
+  return (
+    <div className={style.eventItem}>
+      <div className={`${style.event} flex-[2]`}>
+        <div className={style.eventIcon}>
+          <BsFillCartFill />
+        </div>
+        <div className={style.eventName}>Sale</div>
+      </div>
+      <div className={`${style.eventPrice} flex-[2]`}>
+        <Image
+          src={EthLogo}
+          width={20}
+          height={20}
+          alt="eth"
+          className={style.ethLogo}
+        />
+        <div className={style.eventPriceValue}>{event.price}</div>
+      </div>
+      <div className={`${style.accent} flex-[3]`}>{event.from}</div>
+      <div className={`${style.accent} flex-[3]`}>{event.to}</div>
+      <div className={`${style.accent} flex-[2]`}>{event.date}</div>
+    </div>
+  )
+}
+
+export default EventItem
